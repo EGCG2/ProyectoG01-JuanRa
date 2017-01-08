27e81fda52bb5b1025db483697039c57d1f9d141
juanrarodriguez18 <dragondr3w@gmail.com>
2017-01-08 23:33:21 +0100

    Cambiado nombre paquete del proyecto y añadido código para las clases

---
+++ src/pg01_egc/Resta.java
@@ -0,0 +1,13 @@
+package pg01_egc;
+
+public class App 
+{
+    public static int resta(int n1, int n2){
+		    int result = 0;
+		    
+		    //Restamos los dos numeros
+		    result = n1 - n2;
+		    
+		    return result;
+	}
+}
