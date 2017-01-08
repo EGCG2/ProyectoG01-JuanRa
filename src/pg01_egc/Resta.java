85dd1b83eefc250570ff62d2624c1715c8ad02f7
juanrarodriguez18 <dragondr3w@gmail.com>
2017-01-08 23:49:01 +0100

    Añadido el método suma

--- src/pg01_egc/Resta.java
+++ src/pg01_egc/Resta.java
@@ -10,4 +10,13 @@ public class App
 		    
 		    return result;
 	}
+
+	public static int suma(int n1, int n2){
+		    int result = 0;
+		    
+		    //Sumamos los dos numeros
+		    result = n1 + n2;
+		    
+		    return result;
+	}
 }
