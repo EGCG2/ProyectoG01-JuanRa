f844a6e172b36b9c4cee4851593783d6a099e0d6
juanrarodriguez18 <dragondr3w@gmail.com>
2017-01-08 23:43:31 +0100

    Conflicto resuelto

--- test/pg01_egc/RestaTest.java
+++ test/pg01_egc/RestaTest.java
@@ -10,19 +10,9 @@ public class AppTest{
     
     @Test
     public void Test(){
-<<<<<<< HEAD
 
         System.out.println("Test del método para restar.");
 
         assertEquals(5, Resta.resta(5,0));
     }
 }
-=======
-
-        System.out.println("Test del método para restar.");
-
-        assertEquals(5, Resta.resta(3,0)); //introducimos un conflicto
-    }
-}
-
->>>>>>> rama1
