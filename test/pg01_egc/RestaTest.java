0cee88474c6d7bf3058182fa3a58c23a629dba8b
juanrarodriguez18 <dragondr3w@gmail.com>
2017-01-08 23:40:53 +0100

    Añadido línea para provocar conflicto

--- test/pg01_egc/RestaTest.java
+++ test/pg01_egc/RestaTest.java
@@ -1,38 +1,19 @@
-package com.egc.app;
+package pg01_egc;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import pg01_egc.Resta;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import static org.junit.Assert.*;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+public class AppTest{
+    
+    @Test
+    public void Test(){
+
+        System.out.println("Test del método para restar.");
+
+        assertEquals(5, Resta.resta(3,0)); //introducimos un conflicto
     }
 }
+
